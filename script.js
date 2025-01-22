05db2e83a99c355872165bcf57b12628427a8d76
ItsHariii <sriharisuhas271@gmail.com>
2025-01-21 22:31:47 -0500

    initial
    day 1

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
